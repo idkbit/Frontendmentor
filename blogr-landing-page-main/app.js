2b6141b936254ce599667ea38dd44965883e71ba
Valikhan <ggwpanda@gmail.com>
2021-05-30 00:05:46 +0600

    header in progress

--- blogr-landing-page-main/app.js
+++ blogr-landing-page-main/app.js
@@ -7,18 +7,28 @@ window.addEventListener("click", e => {
   navLinks.forEach(link => {
     if (e.target !== link) {
       link.firstElementChild.classList.remove("nav__arrow_active");
+      link.lastElementChild.classList.remove("nav__arrow_dark_active")
       link.nextElementSibling.classList.remove("dropdown-container_active");
     }
   })
 });
 
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 891) {
+    menu.classList.remove("menu_active");
+    navList.classList.remove("nav__list_active");
+  }
+})
+
 navLinks.forEach(link => {
   link.addEventListener("click", () => {
     link.firstElementChild.classList.toggle("nav__arrow_active");
+    link.lastElementChild.classList.toggle("nav__arrow_dark_active");
     link.nextElementSibling.classList.toggle("dropdown-container_active");
     navLinks.forEach(l => {
       if (link !== l) {
         l.firstElementChild.classList.remove("nav__arrow_active");
+        l.lastElementChild.classList.remove("nav__arrow_dark_active");
         l.nextElementSibling.classList.remove("dropdown-container_active");
       }
     })
